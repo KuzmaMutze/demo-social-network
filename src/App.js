ff2feefa505132ae75a725f059dd853f1775cb95
Matvey <r3dw0r13@yandex.ru>
2021-02-09 07:27:32 +0400

    browserRouter basename app

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
 		}
 
 		return (
-			<BrowserRouter>
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
 				<div className="app-wrapper">
 					<HeaderContainer/>
 					<Nav />
