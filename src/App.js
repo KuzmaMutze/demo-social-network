a88da1aa3bccbf20510bf9f89c3a00c6062455a7
Matvey <r3dw0r13@yandex.ru>
2021-02-09 05:51:48 +0400

    94 react lazy, suspense hoc

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import Nav from './components/Nav/Nav';
-import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import Setting from './components/Setting/Setting';
 import Music from './components/Music/Music';
-import {BrowserRouter, Route, withRouter} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import UsersContainer from './components/Users/UsersContainer';
-import ContentContainer from './components/Content/ContentContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import { connect } from 'react-redux';
 import { initializeApp } from './redax/app-reducer';
 import { compose } from 'redux';
 import Preloader from './components/common/Preloader/Preloader';
+import { withSuspensHoc } from './hoc/withSuspensHoc';
+
+// import Dialogs from './components/Dialogs/Dialogs';
+// import ContentContainer from './components/Content/ContentContainer';
+const Dialogs = React.lazy(() => import('./components/Dialogs/Dialogs'));
+const ContentContainer = React.lazy(() => import('./components/Content/ContentContainer'));
 
 class App extends React.Component {
 
@@ -35,11 +38,11 @@ class App extends React.Component {
 					<Nav />
 					<div className="app-wrapper-content">
 						<Route path="/login" component={ () => <Login/>}/>
-						<Route exact path="/dialogs" component={ () => <Dialogs />}/>
+						<Route exact path="/dialogs" component={ withSuspensHoc(Dialogs) }/>
 						<Route path="/news" component={ () => <News />}/>
 						<Route path="/music" component={ () => <Music />}/>
 						<Route path="/setting" component={ () => <Setting />}/>
-						<Route path="/profile/:userId?" component={ () => <ContentContainer/>}/>
+						<Route path="/profile/:userId?" component={ withSuspensHoc(ContentContainer)}/>
 						<Route path="/users" component={ () => <UsersContainer/>}/>
 					</div>
 				</div>
