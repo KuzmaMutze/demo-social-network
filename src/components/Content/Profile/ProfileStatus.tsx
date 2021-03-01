039810fb5e7952c5640d864266daa53e1fdd9a15
Matvey <r3dw0r13@yandex.ru>
2021-03-01 21:10:54 +0400

    05 type connect, typeStatusComponent, componentDidUpdate, ChangeEvent<HTMLInputElement>

---
+++ src/components/Content/Profile/ProfileStatus.tsx
@@ -0,0 +1,65 @@
+import { type } from "os";
+import React, { ChangeEvent } from "react";
+
+type PropsType = {
+    status: string
+    updateStatusProfile: (status: string) => void
+}
+
+type StateType = {
+    status: string
+    editMode: boolean
+}
+
+class ProfileStatus extends React.Component<PropsType, StateType> {
+
+    state = {
+        editMode: false,
+        status: this.props.status
+    }
+
+    activeteEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    disableEditMode = () => {
+        this.setState({
+            editMode: false
+        })
+        this.props.updateStatusProfile(this.state.status);
+    }
+
+    onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            status: e.currentTarget.value
+        });
+    }
+
+    componentDidUpdate(prevProps: PropsType, prevState: StateType) {
+        debugger
+        if ( prevState.status !== this.props.status ) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                {this.state.editMode ? 
+
+                    <div  onBlur={this.disableEditMode} onDoubleClick={ this.disableEditMode }>
+                        <input onChange={this.onStatusChange} autoFocus={true}  value={this.state.status}></input>
+                    </div>
+
+                    : <div onDoubleClick={ this.activeteEditMode }><span>{this.props.status || "set status"}</span></div>
+                }
+            </div>
+        )
+    }
+    
+};
+export default ProfileStatus;
