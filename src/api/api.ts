62ab443701d559c3b817ddea2007e57c3ac9dbae
Matvey <r3dw0r13@yandex.ru>
2021-03-20 12:39:16 +0400

    10 Type for ALL (API)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,12 @@
+import { PhotosType, ProfileType } from './../types/types';
 import axios, { AxiosResponse } from "axios";
+import { UsersType } from "../types/types";
 
-const instance = axios.create({
-    withCredentials: true,
-    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
-    headers: {
-        "API-KEY": "dfcd1a69-dd5c-48e6-9f01-dbdf80891de4"
-    }
-})
+type ResponseType<D = {}, RC = ResultCodesEnum> = {
+    data: D
+    messages: Array<string>
+    resultCode: RC
+}
 
 let data = (param: any) => {
     return param.then((response:AxiosResponse<any> ) => {
@@ -14,71 +14,91 @@ let data = (param: any) => {
     });
 }
 
+export const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        "API-KEY": "dfcd1a69-dd5c-48e6-9f01-dbdf80891de4"
+    }
+})
+
 export enum ResultCodesEnum {
     Success = 0,
     Error = 1,
     CaptchaIsRequeired = 10
 }
 
-type GetLoginType = {
-    data: {id: number, email: string, login: string}
-    resultCode: ResultCodesEnum
-    message: Array<string>
+type GetLoginDataType = {
+    id: number,
+    email: string,
+    login: string
 }
 
-type LoginResponseType = {
-    data: {userId: number}
-    resultCode: ResultCodesEnum
-    message: Array<string>
+type LoginResponseDataType = {
+    userId: number
+}
+
+type GetUsersType = {
+    items: Array<UsersType>
+    totalCount: number
+    error: string | null
+}
+
+type SavePhotoResponseDataType = {
+    photos: PhotosType
+}
+
+type GetCaptchaURLResponseType = {
+    url: string
 }
 
 export const usersAPI = {
     // header auth
     getLogin () {
-        return data(instance.get<GetLoginType>(`auth/me`))
+        return data(instance.get<ResponseType<GetLoginDataType>>(`auth/me`))
     },
     login (email: string, password: string, rememberMe: boolean = false, captcha: null | string = null) {
-        return data(instance.post<LoginResponseType>(`auth/login`, {email, password, rememberMe, captcha}))
+        return data(instance.post<ResponseType<LoginResponseDataType>>(`auth/login`, {email, password, rememberMe, captcha}))
     },
     logout () {
         return data(instance.delete(`auth/login`))
     },
     // users
     getUsers(currentPage: number = 1, pageSize: number = 5) {
-        return data(instance.get(`users?page=${currentPage}&count=${pageSize}`))
+        return data(instance.get<GetUsersType>(`users?page=${currentPage}&count=${pageSize}`))
     },
     savePhoto(photoFile: any) {
         const formData = new FormData();
         formData.append("image", photoFile);
 
-        return data(instance.put(`profile/photo`, formData, {
+        return data(instance.put<ResponseType<SavePhotoResponseDataType>>(`profile/photo`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
         }))
     },
     follow(userId: number) {
-        return data(instance.post(`follow/${userId}`))
+        return data(instance.post<ResponseType>(`follow/${userId}`))
     },
     unFollow(userId: number) {
-        return data(instance.delete(`follow/${userId}`))
+        return data(instance.delete(`follow/${userId}`)) as Promise<ResponseType>
     },
     // content profile
     getProfile(userId: number) {
-        return data(instance.get(`profile/${userId}`)) 
+        return data(instance.get<ProfileType>(`profile/${userId}`))
     },
-    setProfileInfo(profileInfo: string) {
-        return data(instance.put(`profile/`, profileInfo))
+    setProfileInfo(profileInfo: ProfileType) {
+        return data(instance.put<ResponseType>(`profile/`, profileInfo))
     },
     // status
     getStatus(userId: number) {
-        return data(instance.get(`profile/status/${userId}`))
+        return data(instance.get<string>(`profile/status/${userId}`))
     },
     updateStatus(status: string) {
-        return data(instance.put(`profile/status/`, {status: status}))
+        return data(instance.put<ResponseType>(`profile/status/`, {status: status}))
     },
     // security
     getCaptcha() {
-        return data(instance.get(`security/get-captcha-url`))
+        return data(instance.get<GetCaptchaURLResponseType>(`security/get-captcha-url`))
     }
 }
